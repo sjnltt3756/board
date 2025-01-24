b62e879e8d3b665a5cb9c99cc54930c10bda47d5
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-25 00:06:06 +0900

    BoardRepository

---
+++ src/main/java/com/board/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.board.repository;
+
+import com.board.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
+
+}
