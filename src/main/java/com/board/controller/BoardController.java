6cbd992183c8550055d7adc2434561ba11b49439
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-25 00:05:14 +0900

    BoardController postMapping -> save 작성

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -1,15 +1,26 @@
 package com.board.controller;
 
+import com.board.dto.BoardDTO;
+import com.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+    private final BoardService boardService;
+
     @GetMapping("/save")
     public String saveFrom(){
-
         return "save";
+    }
+    // 동일한 주소여도 post와 get일 경우에는 가능하지만 같은 mapping이라면 Exception 발생
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "index";
     }
 }
