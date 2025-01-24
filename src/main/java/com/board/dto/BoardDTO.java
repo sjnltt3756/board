8c749283aaf2bd456bcf442affafe254bb9c5518
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-25 00:05:21 +0900

    DTO 작성

---
+++ src/main/java/com/board/dto/BoardDTO.java
@@ -0,0 +1,24 @@
+package com.board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+// DTO(Data Transfer Object), VO, Bean -> 데이터를 전송할 때 사용하는 객체
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class BoardDTO {
+
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits; // 게시글 조회수
+    private LocalDateTime boardCreatedTime;
+    private LocalDateTime boardUpdateTime;
+
+}
