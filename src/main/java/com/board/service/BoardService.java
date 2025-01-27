78d432a8f65618203c578d7372f7be2baa1eb268
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-27 15:23:34 +0900

    게시글 목록 service

--- src/main/java/com/board/service/BoardService.java
+++ src/main/java/com/board/service/BoardService.java
@@ -6,6 +6,9 @@ import com.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // DTO -> Entity (Entity Class)
 // Entity -> Dto (DTO Class)
 
@@ -18,5 +21,14 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity saveEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(saveEntity);
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity : boardEntityList){
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
