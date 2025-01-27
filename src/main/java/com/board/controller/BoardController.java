57ccef61a9d6840baeca023ac7df10999c12afa7
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-27 15:23:25 +0900

    게시글 목록 controller

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -4,7 +4,10 @@ import com.board.dto.BoardDTO;
 import com.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +25,13 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        // DB에서 전체 데이터 게시글을 가져와서 index.html에 보여준다.
+        List<BoardDTO> boardDTOList =  boardService.findAll();
+        model.addAttribute("boardList",boardDTOList);
+        return "list";
     }
 }
