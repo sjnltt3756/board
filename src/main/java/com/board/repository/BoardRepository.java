a7be5cf9e290081199324003eecadcf44328f92f
sjnltt3756 <sjnltt3756@gmail.com>
2025-01-29 21:42:19 +0900

    조회수 업데이트

--- src/main/java/com/board/repository/BoardRepository.java
+++ src/main/java/com/board/repository/BoardRepository.java
@@ -2,9 +2,17 @@ package com.board.repository;
 
 import com.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
+    // update board_table set board_hits = board_hits+1 where id=?  (mySQL 기준)
+
+    @Modifying  // update나 delete 같은 쿼리를 실행할 때는 추가적으로 붙여줘야 함
+    @Query(value = "update BoardEntity b set b.boardHits = b.boardHits+1 where b.id = :id") // (Entity기준)
+    void updateHits(@Param("id") Long id);
 
 }
