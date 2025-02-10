821252f63120ec4841338b9c754718fdeceef007
sjnltt3756 <sjnltt3756@gmail.com>
2025-02-10 20:24:25 +0900

    페이징 마무리

--- src/main/java/com/board/dto/BoardDTO.java
+++ src/main/java/com/board/dto/BoardDTO.java
@@ -23,6 +23,14 @@ public class BoardDTO {
     private LocalDateTime boardUpdateTime;
 
 
+    public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
+        this.id = id;
+        this.boardWriter = boardWriter;
+        this.boardTitle = boardTitle;
+        this.boardHits = boardHits;
+        this.boardCreatedTime = boardCreatedTime;
+    }
+
     public static BoardDTO toBoardDTO(BoardEntity boardEntity){
         BoardDTO boardDTO = new BoardDTO();
         boardDTO.setId(boardEntity.getId());
