99007a5262ec8d9b95d83552ad2afe510fc83942
sjnltt3756 <sjnltt3756@gmail.com>
2025-02-12 21:03:05 +0900

    save.html에서 파일첨부 받는 dto 매개변수 작성

--- src/main/java/com/board/dto/BoardDTO.java
+++ src/main/java/com/board/dto/BoardDTO.java
@@ -2,6 +2,7 @@ package com.board.dto;
 
 import com.board.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -22,6 +23,10 @@ public class BoardDTO {
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdateTime;
 
+    private MultipartFile boardFile;    // MultipartFile 타입으로 save.html -> controller 파일을 담는다.
+    private String originalFileName;    // 원본 파일 이름
+    private String storedFileName;      // 서버 저장용 파일 이름
+    private int fileAttached;           // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
