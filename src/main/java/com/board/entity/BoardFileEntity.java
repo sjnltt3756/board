5326806eb392afa8524008d709f29f062795d04f
sjnltt3756 <sjnltt3756@gmail.com>
2025-02-26 11:07:59 +0900

    타입 변경

--- src/main/java/com/board/entity/BoardFileEntity.java
+++ src/main/java/com/board/entity/BoardFileEntity.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class BoardFileEntity extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column
     private String originalFileName;
