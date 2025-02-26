2c201725e8b679d997cc80596531f097c5602f55
sjnltt3756 <sjnltt3756@gmail.com>
2025-02-26 11:12:03 +0900

    CommentDTO  작성

---
+++ src/main/java/com/board/dto/CommentDTO.java
@@ -0,0 +1,18 @@
+package com.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO {
+    private Long id;
+    private String commentWriter;
+    private String commentContents;
+    private Long boardId;
+    private LocalDateTime commentSavedTime;
+}
