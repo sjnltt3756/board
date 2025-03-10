8cf60cf0c0026c0d581126107c2eb4be654f3ece
sjnltt3756 <sjnltt3756@gmail.com>
2025-03-10 18:52:45 +0900

    댓글 dto

--- src/main/java/com/board/dto/CommentDTO.java
+++ src/main/java/com/board/dto/CommentDTO.java
@@ -1,5 +1,6 @@
 package com.board.dto;
 
+import com.board.entity.CommentEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,5 +15,16 @@ public class CommentDTO {
     private String commentWriter;
     private String commentContents;
     private Long boardId;
-    private LocalDateTime commentSavedTime;
+    private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toCommentDTO(CommentEntity commentEntity, Long boardId) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentEntity.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+        //commentDTO.setId(commentEntity.getBoardEntity().getId()); // Service 매서드에 반드시 @Transaction을 붙여주기
+        commentDTO.setBoardId(boardId);
+        return commentDTO;
+    }
 }
