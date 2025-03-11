805dfa96ff4d42316075a9d234b2b1cc6e2e08aa
sjnltt3756 <sjnltt3756@gmail.com>
2025-03-11 18:32:29 +0900

    댓글 목록 가져오기

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.board.controller;
 
 import com.board.dto.BoardDTO;
+import com.board.dto.CommentDTO;
 import com.board.service.BoardService;
+import com.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,7 +20,7 @@ import java.util.List;
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
-
+    private final CommentService commentService;
     @GetMapping("/save")
     public String saveFrom(){
         return "save";
@@ -50,6 +52,10 @@ public class BoardController {
          */
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
+        /* 댓글 목록 가져오기 */
+        List<CommentDTO> commentDTOList = commentService.findAll(id);
+        model.addAttribute("commentList",commentDTOList);
+
         model.addAttribute("board", boardDTO);
         model.addAttribute("page", pageable.getPageNumber());
         return "detail";
